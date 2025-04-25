0959accb04d169a90d2faeadd5e5f2ff2583f548
Nikita2993-Z <zinchenko.nikita93@gmail.com>
2025-04-25 15:45:33 +0300

    tech-skillls, update width and infinite string

--- src/js/tech-skills.js
+++ src/js/tech-skills.js
@@ -1,3 +1,4 @@
-const marquee = document.querySelector('.marquee');
-const content = marquee.innerHTML;
-marquee.innerHTML += content;
+const marquees = document.querySelectorAll('.marquee');
+marquees.forEach(marquee => {
+  marquee.innerHTML += marquee.innerHTML;
+});
